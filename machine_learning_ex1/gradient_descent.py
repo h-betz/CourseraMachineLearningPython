e3f159bc39c7d4555369e1ccb64213dd1e20ad92
h-betz <hhb311@gmail.com>
2018-02-12 21:40:13 -0500

    Worked on gradient descent

--- machine_learning_ex1/gradient_descent.py
+++ machine_learning_ex1/gradient_descent.py
@@ -1,4 +1,13 @@
-import numpy
+from machine_learning_ex1.compute_cost import compute_cost
+import numpy as np
 
 def gradient_descent(x, y, theta, alpha, iterations):
-	pass
+	m = y.size
+	j_history = np.zeros((iterations,1))
+
+	for j in range(1,iterations):
+		h = x.dot(theta)
+		delta = (1 / m) * (x.T.dot(h-y))
+		theta = theta - alpha * delta
+		j_history[j] = compute_cost(x, y, theta)
+	return theta, j_history
